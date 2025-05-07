7f2c27db1342c8e2024cc1c23e56a42348c6d591
Manny <manny.blum@gmail.com>
2025-05-07 13:49:17 -0400

    added resume file and download link

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -5,6 +5,13 @@ export default function Intro() {
         <h1>Manny Blum</h1>
         <h2>Front-End/UI/UX Engineer</h2>
       </header>
+      <a
+        className="download-btn"
+        href="./assets/manny-blum-2025-resume-v2.pdf"
+        download
+      >
+        Download Resume
+      </a>
     </section>
   );
 }
