ff4c643386449b65a64878370c4f0003c85a3817
Manny <manny.blum@gmail.com>
2025-05-08 16:50:19 -0400

    setup react-router and a temporary page

---
+++ src/components/Conversions/Conversions.tsx
@@ -0,0 +1,3 @@
+export default function Conversions() {
+  return <h1>Conversions</h1>;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
+import "./index.css";
+import App from "./App.tsx";
+import { Conversions } from "./components/Conversions/";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="conversions" element={<Conversions />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
